6123277405bb48dc22e7b1f5b9eb4b685acc25d5
axell-brendow <breno.axel@gmail.com>
2019-11-24 11:03:02 -0300

    Deixa as páginas de index e 404 mais bonitas
    1 - Coloquei um .svg com o título do Star Wars na index.
    2 - Coloquei um .svg com um stormtrooper triste na página de 404.

--- src/pages/Error404/index.js
+++ src/pages/Error404/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import './styles.css';
 
 function Error404() {
     return (
         <div className="Error404">
-            <h1>Page not found</h1>
+            <h1 className={"title"}>Sorry, page not found</h1>
+            <img src={ require("../../assets/sad_stormtrooper.svg") } alt={"Sad stormtrooper"}
+                    className={"sad-image"}/>
             <br/>
-            <Link to={"/"}>
+            <span className={"bg-dark link-container"}>
                 {/*<span className="icon">
                     <i className="fa fa-long-arrow-left"/>
                 </span>*/}
-
-                <span>Back</span>
-            </Link>
+                <Link to={"/"} className="links">Back</Link>
+            </span>
         </div>
     );
 }
