2bd734dbc78f26af150ec8af845ce37620840bc1
axell-brendow <breno.axel@gmail.com>
2019-12-05 05:45:23 -0300

    Inicia busca dos dados da API

--- src/App.js
+++ src/App.js
@@ -11,8 +11,88 @@ import Home from "./pages/Home";
 import Error404 from "./pages/Error404";
 import Category from "./pages/Category";
 import Item from "./pages/Item";
+import axios from 'axios';
+
+export const API_BASE = 'https://swapi.co/api/';
 
 const App = () => {
+
+    /**
+     * Até o momento, a organização que pensei foi o objeto apiData ser do tipo:
+     *
+     * {
+     *     url: dado,
+     *     url: dado,
+     *     url: dado,
+     *     url: dado,
+     * }
+     *
+     * Isso porque a api sempre traz as urls das coisas, então basta acessar o
+     * objeto apiData usando a url como chave que ele trará os dados caso já tenham
+     * sido pegos.
+     */
+    const [apiData, setApiData] = useState({});
+
+    function fetchUrl(url = API_BASE)
+    {
+        return new Promise(
+            (resolve, reject) =>
+            {
+                if (apiData[url]) resolve(apiData[url]);
+
+                else axios.get(url).then(
+                    (response) =>
+                    {
+                        setApiData({...apiData, [url]: response.data});
+                        resolve(response.data);
+                    }
+                )
+                .catch((error) => console.log(error));
+            }
+        );
+    }
+
+    function fetchCategory(categoryName, page = 1)
+    {
+        return new Promise(
+            (resolve, reject) =>
+            {
+                const pageQuery = page === 1 ? '' : `?page=${page}`;
+                const url = `${API_BASE}${categoryName}/${pageQuery}`;
+
+                fetchUrl(url).then((data) => {
+                    resolve(data);
+
+                    /*const newApiData = {...apiData};
+
+                    // Além de fazer o cache da url da categoria, faz o cache das urls
+                    // de cada um dos itens dela
+                    data.results.forEach((result) => newApiData[result.url] = result);
+
+                    setApiData(newApiData);*/
+
+                    console.log(data);
+                });
+            }
+        );
+    }
+
+    function fetchCategoryItem(categoryName, id)
+    {
+        return new Promise(
+            (resolve, reject) =>
+            {
+                const url = `${API_BASE}${categoryName}/${id}`;
+
+                fetchUrl(url).then((data) => {
+                    resolve(data);
+                    console.log(data);
+                });
+            }
+        );
+    }
+
+    useEffect(() => { fetchCategory('people'); }, [apiData]);
 
     /*const [darkModeActived, setDarkModeActived] = useState(false);
 
