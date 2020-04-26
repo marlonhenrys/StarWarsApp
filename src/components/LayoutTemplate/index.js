69be764a3d83d39e4fd813d4b04c03f626c742c7
axell-brendow <breno.axel@gmail.com>
2020-04-26 12:49:36 -0300

    Remove unused code

--- src/components/LayoutTemplate/index.js
+++ src/components/LayoutTemplate/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import './styles.css';
 import Header from "../Header";
 import SideNav from "../SideNav";
@@ -15,26 +15,8 @@ $(document).ready(fixContent);
 
 const LayoutTemplate = ({ headerTitle, children, darkModeActived, changeTheme }) => {
 
-    // const [darkModeActived, setDarkModeActived] = useState(false);
-
     fixContent();
     useEffect(fixContent, []);
-
-    // useEffect(() => {
-    //     const root = document.documentElement;
-
-    //     if (darkModeActived) {
-    //         root.style.setProperty('--color-bg-main', '#000');
-    //         root.style.setProperty('--color-bg-items', '#444');
-    //         root.style.setProperty('--color-font-standard', '#FFF');
-    //         root.style.setProperty('--color-font-feature', 'rgb(231, 212, 28)');
-    //     } else {
-    //         root.style.setProperty('--color-bg-main', '#CCC');
-    //         root.style.setProperty('--color-bg-items', '#FFF');
-    //         root.style.setProperty('--color-font-standard', '#000');
-    //         root.style.setProperty('--color-font-feature', 'rgb(88, 90, 155)');
-    //     }
-    // }, [darkModeActived]);
 
     return (
         <div className="LayoutTemplate">
