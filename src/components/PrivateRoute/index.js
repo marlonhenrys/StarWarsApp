cd8c987ca9201faa6062b4a29fbc25574a9ee6a8
axell-brendow <breno.axel@gmail.com>
2020-04-26 12:49:27 -0300

    Improve PrivateRoute component

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -1,17 +1,27 @@
-import React, { Fragment } from 'react';
-import { Redirect } from 'react-router-dom';
-import {isUserLogged} from "../../utils/authAdministration";
-import './styles.css';
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import { isUserLogged } from "../../utils/authAdministration";
+import "./styles.css";
 import routes from "../../utils/routes";
 
-const PrivateRoute = (props) => (
-    <Fragment>
-        {/* The idea is that the children of this component are like protected routes */}
-        { isUserLogged() ? props.children : <Redirect to={{
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isUserLogged() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
             pathname: routes.login,
-            state: { authMsg: "You need to be authenticated to view this content." }
-        }} /> }
-    </Fragment>
+            state: {
+              authMsg: "You need to be authenticated to view this content.",
+            },
+          }}
+        />
+      )
+    }
+  />
 );
 
 export default PrivateRoute;
